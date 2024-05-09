69a445ff738c5b56f9d1a8445ad310f2b5bedba2
CommanderTempest <krukovtempest@gmail.com>
2024-05-09 15:33:28 -0500

    Added JP translation to Spellblade page

--- src/pages/spellblade.js
+++ src/pages/spellblade.js
@@ -6,5 +6,6 @@ export default function SpellBlade() {
         <h1>Download the current version of Spellblade from below</h1>
         <h2>Current Version: 0.2</h2>
         <a className="download" href={spellbladeDownload} download>Spellblade Download</a>
+        <h2>ゲームをダウンロードするには、上のリンクをクリックしてください。</h2>
     </div>);
 }
